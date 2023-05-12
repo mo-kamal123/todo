c058e2ca37d77388c4d37b8f452496d16eb8817b
mostafa-kamal123@gmail.com <Mostafa-kamal123@github.com>
2023-05-12 15:34:45 +0300

    frist pro

---
+++ to.js
@@ -0,0 +1,31 @@
+document.querySelector('#bush').onclick = function (){
+    if (document.querySelector('#newtask input').
+    value.length == 0){
+        alert("Enter your task please ..!")
+    } else {
+        document.querySelector('#tasks').innerHTML
+        += `
+            <div class="task">
+                    <span id= "taskname">
+                        ${
+                            document.querySelector('#newtask input').value
+                        }
+                    </span>
+                    <button class="delete"><i class="fa-solid fa-trash"></i></button>
+            </div>`;
+                var current = document.querySelectorAll('.delete');
+                for (var i=0 ; i < current.length ; i++){
+                    current[i].onclick = function(){
+                        this.parentNode.remove();
+                    }
+                }
+                var tasks =document.querySelectorAll('.task');
+                for(var i=0; i<tasks.length; i++) {
+                        tasks[i].onclick = function () {
+                            this.classList.toggle('completed')
+                        }
+                }
+                document.querySelector('#newtask input').value = "";
+    }
+}
+
